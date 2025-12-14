fc30bcf05831293e34c402cff7ba944f23776c3b
Pixel-Code-Hub206 <mayanklan206@gmail.com>
2025-12-14 16:16:43 +0530

    Added a bottom navigation bar for the respective pages of the application.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview_plus/device_preview_plus.dart';
-import 'package:musehub/pages/library_page.dart';
+import 'package:musehub/pages/muse_hub_root.dart';
 
 void main() {
   runApp(
     DevicePreview(
-      enabled: kDebugMode,
+      enabled: false,   //If AVD's fail to work smoothly
         builder: (context) => MaterialApp(
           useInheritedMediaQuery: true,
           builder: DevicePreview.appBuilder, 
-          home: Home(),
+          home: MuseHubRoot(),
         ),
     ),
   );
---
+++ lib/pages/muse_hub_root.dart
@@ -0,0 +1,73 @@
+//The first page to land in after the splash screen
+import 'package:flutter/material.dart';
+import 'package:musehub/pages/favorites_page.dart';
+import 'package:musehub/pages/library_page.dart';
+import 'package:musehub/pages/now_playing_page.dart';
+import 'package:musehub/pages/playlists_page.dart';
+import 'package:musehub/pages/settings_page.dart';
+
+class MuseHubRoot extends StatefulWidget {
+  const MuseHubRoot({super.key});
+
+  @override
+  State<MuseHubRoot> createState() => _MuseHubRootState();
+}
+
+class _MuseHubRootState extends State<MuseHubRoot> {
+  int _currentIndex = 0;
+
+  final List<Widget> _screens = [
+    LibraryPage(),
+    FavoritesPage(),
+    PlaylistsPage(),
+    SettingsPage()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Root File',
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+      body: _screens[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+
+        selectedItemColor: Colors.greenAccent,
+        unselectedItemColor: Colors.grey,
+
+        backgroundColor: Colors.black,
+
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Library',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorites',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.playlist_play),
+            label: 'Playlist',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
