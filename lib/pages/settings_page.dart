e6c9fa5ba5ade91891625256bd6ea0f612599a43
Pixel-Code-Hub206 <mayanklan206@gmail.com>
2025-12-16 01:16:07 +0530

    Initialized the base structure of the Settings page to work with in the future.

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -16,31 +16,142 @@ class SettingsPage extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+            child: Text(
+                'Appearance',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
           ListTile(
             leading: Icon(Icons.account_circle_outlined),
-            title: Text('Appearance'),
-            subtitle: Text('Dark Theme - Toggle')
+            title: Text('Dark Theme - Always On'),
+            onTap: () {
+              print('Toggle theme tapped!');
+            },
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+            child: Text(
+              'Audio',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
           ),
           ListTile(
             leading: Icon(Icons.audiotrack_outlined),
-            title: Text('Audio'),
-            subtitle: Text('Gapless playback'),
+            title: Text('Gapless playback'),
+            onTap: () {
+              print('Uses a switch!');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.audiotrack_outlined),
+            title: Text('Normalize Volume'),
+            onTap: () {
+              print('Uses a switch!');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.audiotrack_outlined),
+            title: Text('CrossFade Duration'),
+            onTap: () {
+              print('Leads to another page!');
+            },
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+            child: Text(
+              'Library',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
           ),
           ListTile(
             leading: Icon(Icons.library_music_outlined),
-            title: Text('Library'),
-            subtitle: Text('Rescan disk'),
+            title: Text('Rescan disk'),
+            onTap: () {
+              print('Dialog (Confirmation)');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.audiotrack_outlined),
+            title: Text('Clear cached artwork'),
+            onTap: () {
+              print('Dialog (Confirmation)');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.audiotrack_outlined),
+            title: Text('Choose scan directories'),
+            onTap: () {
+              print('New Page');
+            },
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+            child: Text(
+              'Playback',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
           ),
           ListTile(
             leading: Icon(Icons.volume_up_outlined),
-            title: Text('Playback'),
-            subtitle: Text('Autoplay controls'),
+            title: Text('Continue from last played track'),
+            onTap: () {
+              print('Playback Controls tapped!');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.volume_up_outlined),
+            title: Text('Auto-play next track'),
+            onTap: () {
+              print('Playback Controls tapped!');
+            },
+          ),
+
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+            child: Text(
+              'App Info',
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
           ),
           ListTile(
             leading: Icon(Icons.info_outline),
-            title: Text('App Info'),
-            subtitle: Text('About MuseHub'),
-          )
+            title: Text('About MuseHub'),
+            onTap: () {
+              print('Dialog Box');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.audiotrack_outlined),
+            title: Text('Developer Link'),
+            onTap: () {
+              print('External Link');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.audiotrack_outlined),
+            title: Text('App Version : v0.1.0 (alpha)'),
+          ),
         ],
       ),
     );
