ffeaa21426c1beae882e9d0cf3fb26a6ebc34e0b
Pixel-Code-Hub206 <mayanklan206@gmail.com>
2025-12-22 19:01:21 +0530

    Made the base 'Song model' and the base Library page using the listview builder.

--- lib/models/song_model.dart
+++ lib/models/song_model.dart
@@ -1,1 +1,11 @@
-//Defining the song model
+import 'package:flutter/material.dart';
+import 'package:musehub/pages/library_page.dart';
+
+ class Song {
+   final String songName;
+   final String artistName;
+   final String artworkUri;     //Album art path
+
+   Song({required this.songName, required this.artistName, required this.artworkUri});
+
+ }
--- lib/pages/library_page.dart
+++ lib/pages/library_page.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:musehub/models/song_model.dart';
 
 class LibraryPage extends StatelessWidget {
   const LibraryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<Song> songs = [
+      Song(songName: "Undertale", artistName: "Toby Fox", artworkUri: "The Dog"),
+      Song(songName: "Shooting Stars", artistName: "NCS", artworkUri: "The Sky"),
+      Song(songName: "Bury the Light", artistName: "Cassey Edwards", artworkUri: "The Dog"),
+      Song(songName: "Plumbing up", artistName: "Mario", artworkUri: "Bowser"),
+      Song(songName: "You're too Slow!", artistName: "Sonic", artworkUri: "Mr Beast"),
+    ];    //Dummy Data
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -14,7 +23,25 @@ class LibraryPage extends StatelessWidget {
           ),
         ),
       ),
-      body: Text('The Library Page'),
+      body: ListView.builder(
+        itemCount: songs.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            leading: SizedBox(
+            width: 48,
+            child: Text(songs[index].artworkUri),
+          )
+          ,
+            title: Text(songs[index].songName),
+            subtitle: Text(songs[index].artistName),
+            onTap: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Tapped ${songs[index].songName}')),
+              );
+            },
+          );
+        }
+      ),
     );
   }
 }
